8c2f1995eac63309b1098248deaeff9805292b69
lmoreiram <lucas.medino@neurobots.com.br>
2023-11-06 15:42:01 -0300

    fixed missing matplotlib import

--- angle_calculator.py
+++ angle_calculator.py
@@ -6,11 +6,13 @@ def calculate_angle(v1, v2, joint):
     dot_product = np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
     angle_radians = np.arccos(np.clip(dot_product, -1.0, 1.0))  # Clip for stability
     angle_degrees = np.degrees(angle_radians)
+
+    cross_product = v1[0] * v2[1] - v1[1] * v2[0]
     
     if joint == 'hip':
         angle_direction = -1 if v2[0]<0 else 1
     else:
-        angle_direction = 1 
+        angle_direction = 1 if cross_product > 0 else -1 
     
     angle = angle_direction*angle_degrees
 
